903afc1831a1beef7b86e329e348df3aac918e1e
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-06-26 06:49:04 -0500

    just adjusting positioning

--- index.js
+++ index.js
@@ -64,9 +64,11 @@ inquirer
     
     function genSvg(shape) {
         return`
-      <svg width="100" height="100">
+      <svg  version="1.1" width="300" height="200"
+      xmlns="http://www.w3.org/2000/svg">
+      
         ${shape.render()}
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${shape.textColor}" />${shape.text}</text>
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${shape.textColor}" >${shape.text}</text>
         </svg>
         `
     
